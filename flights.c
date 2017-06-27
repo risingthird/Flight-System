5765c0cdb2c5bd28007c42a4a662395981fdf2f4
Yang Bowen <gloriayangbowen@gmail.com>
2017-06-27 22:19:41 +0000

    flights.c edited online with Bitbucket

--- flights.c
+++ flights.c
@@ -170,7 +170,7 @@ void printAirports(flightSys_t* s) {
  // Bowen
 void addFlight(airport_t* src, airport_t* dst, timeHM_t* departure, timeHM_t* arrival, int cost) {
     if (src!=NULL){
-    	flight_t* newFlight=(flight_t* ) malloc(sizeof(flight_t));
+    	//flight_t* newFlight=(flight_t* ) malloc(sizeof(flight_t));
         
         if (!newFlight){
     		allocation_failed();
