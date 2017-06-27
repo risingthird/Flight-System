0ef35bfd3ebfaccf31cf1d9691741eeb78278f3d
Jiaping Wang <nflshc2020@gmail.com>
2017-06-27 01:39:00 +0000

    flights.c edited online with Bitbucket

--- flights.c
+++ flights.c
@@ -281,6 +281,7 @@ int validateFlightPath(flight_t** flight_list, char** airport_name_list, int sz)
     int totalCost = 0;
     int i = 0;
     while(i<sz){
+        printf("here2\n");
         if(*(flight_list+i+1) == NULL) break;
         else if(!isAfter(&((*(flight_list+i+1))->departure), &((*(flight_list+i))->arrival)) && !isEqual(&((*(flight_list+i+1))->arrival),&((*(flight_list+i))->departure)))
             {printf("here1\n");return -1;} // return -1 if the next flight doesn't depart after or at the same time with the former one
