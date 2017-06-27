97c95e9c768a0fb4063fc6ae36b7433ce1b6b000
Yang Bowen <gloriayangbowen@gmail.com>
2017-06-27 00:08:15 +0000

    flights.c edited online with Bitbucket

--- flights.c
+++ flights.c
@@ -178,6 +178,7 @@ void addFlight(airport_t* src, airport_t* dst, timeHM_t* departure, timeHM_t* ar
     if (src!=NULL){
         printf("here2\n");
     	flight_t* newFlight=(flight_t* ) malloc(sizeof(flight_t));
+        newFlight->destination=(airport_t* ) malloc(sizeof(airport_t));
         printf("here9\n");
         if (!newFlight){
             printf("here7\n");
