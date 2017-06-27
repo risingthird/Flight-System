62380a88dbcf86b4eee8b72bf9e84057fe01442d
Yang Bowen <gloriayangbowen@gmail.com>
2017-06-27 22:52:24 +0000

    flights.c edited online with Bitbucket

--- flights.c
+++ flights.c
@@ -62,7 +62,7 @@ flightSys_t* createSystem() {
 flight_t* createFlight(airport_t* dest, timeHM_t dep, timeHM_t arr, int c) {
    // Replace this line with your code
    flight_t* newFlight = (flight_t*) malloc(sizeof(flight_t));
-   if(!newFlight) allocation_failed();
+   //if(!newFlight) allocation_failed();
    else{
        //newFlight->destination = (airport_t*) malloc(sizeof(airport_t));
        if(!newFlight->destination) allocation_failed();
