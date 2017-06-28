48d7242248b7bbe8fb415b01f8f37be664cf6eaa
Jiaping Wang <nflshc2020@gmail.com>
2017-06-28 03:01:16 +0000

    flights.c edited online with Bitbucket

--- flights.c
+++ flights.c
@@ -64,8 +64,9 @@ flight_t* createFlight(airport_t* dest, timeHM_t dep, timeHM_t arr, int c) {
    flight_t* newFlight = (flight_t*) malloc(sizeof(flight_t));
    if(!newFlight) allocation_failed();
    else{
-       newFlight->destination = (airport_t*) malloc(sizeof(airport_t));
-       if(!newFlight->destination) allocation_failed();
+       //newFlight->destination = (airport_t*) malloc(sizeof(airport_t));
+       //if(!newFlight->destination) allocation_failed();
+       if(!dest) {free(newFlight); return NULL;}
        newFlight->destination = dest;
        newFlight->departure = dep;
        newFlight->arrival = arr;
